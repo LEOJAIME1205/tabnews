16c2d8004bc9de2550ed474c540867dc26b48a9b
leo_jaime <12verideanavascomselos@gmail.com>
2026-04-18 22:57:33 -0300

    fix: adiciona `outputFileTracingIncludes` para arquivos de migração

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["node-pg-migrate"],
+  outputFileTracingIncludes: {
+    "/api/v1/migrations": ["./infra/migrations/**/*"],
+  },
 };
 
 module.exports = nextConfig;
